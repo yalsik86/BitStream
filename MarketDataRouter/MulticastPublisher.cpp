69fabc99f9dd5cf1000cc2ec5a887e4ed25521f5
yalsik <msdegoat@gmail.com>
2025-09-11 03:59:08 +0530

    decouple router and multicast publisher

--- MarketDataRouter/MulticastPublisher.cpp
+++ MarketDataRouter/MulticastPublisher.cpp
@@ -3,8 +3,37 @@
 MulticastPublisher::MulticastPublisher() : socket(ioc, udp::endpoint(udp::v4(), 0)), multicast_endpoint(net::ip::make_address_v4("239.255.255.1"), 9000) {
     socket.set_option(net::ip::multicast::hops(5));
     socket.set_option(net::ip::multicast::enable_loopback(true));
-    net::ip::address_v4 local_if = net::ip::make_address_v4("192.168.29.222");
+    net::ip::address_v4 local_if = net::ip::make_address_v4("192.168.29.225");
     socket.set_option(net::ip::multicast::outbound_interface(local_if));
+}
+
+void MulticastPublisher::start() {
+    running = true;
+    worker = std::jthread([this]() {
+        while(running) {
+            std::vector<uint8_t> buffer;
+            {
+                std::unique_lock<std::mutex> lock(multicast_mtx);
+                cv.wait(lock, [&]() {
+                    return !multicastQ.empty() || !running;
+                });
+                if(!running) break;
+
+                buffer = std::move(multicastQ.front());
+                multicastQ.pop();
+            }
+
+            publish(buffer);
+        }
+    });
+}
+
+void MulticastPublisher::push(std::vector<uint8_t>& buffer) {
+    {
+        std::lock_guard<std::mutex> lock(multicast_mtx);
+        multicastQ.push(std::move(buffer));
+    }
+    cv.notify_one();
 }
 
 void MulticastPublisher::publish(const std::vector<uint8_t>& buffer) {
--- MarketDataRouter/MulticastPublisher.hpp
+++ MarketDataRouter/MulticastPublisher.hpp
@@ -1,8 +1,12 @@
 #pragma once
 #include <boost/asio.hpp>
 #include <iostream>
+#include <condition_variable>
+#include <mutex>
 #include <cstdint>
 #include <vector>
+#include <queue>
+#include <thread>
 
 namespace net = boost::asio;
 using udp = net::ip::udp;
@@ -10,10 +14,18 @@ using udp = net::ip::udp;
 class MulticastPublisher {
   public:
     MulticastPublisher();
+    void start();
+    void push(std::vector<uint8_t>& buffer);
     void publish(const std::vector<uint8_t>& buffer);
 
   private:
     net::io_context ioc;
     udp::endpoint multicast_endpoint;
     udp::socket socket;
+
+    bool running = false;
+    std::jthread worker;
+    std::mutex multicast_mtx;
+    std::condition_variable cv;
+    std::queue<std::vector<uint8_t>> multicastQ;
 };
